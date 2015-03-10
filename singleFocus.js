40d204ea3f79fb2f05c3355544c7b54559451496
matthewcv <accounts@matthewcv.com>
2015-03-10 21:25:30 +0000

    adding the single focus thing

---
+++ singleFocus.js
@@ -0,0 +1,27 @@
+(function(){
+    
+    
+    console.log('this is the singleFocus component')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var questions = [
+        {
+            text:"this is the first thing that I want to ask you",
+            type:"string"
+        },
+        {
+            text:"how many days since the last time you left the state you live in?",
+            type:"number"
+        }
+    ]
+    
+    
+})()
