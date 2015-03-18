d4f568c3e5189b226e9cdbfa947777bbe30e3c53
matthewcv <accounts@matthewcv.com>
2015-03-17 22:04:59 -0500

    narrow focus stuff

--- narrowFocus.js
+++ narrowFocus.js
@@ -3,16 +3,78 @@
 (function () {
   document.addEventListener("DOMContentLoaded", start);
 
-
+    var nowIdx = -1;
 
     function start() {
+
+
         initHbs();
         buildUi();
+
+        document.addEventListener("keyup", function (ev) {
+            //console.log(ev.keyCode);
+            if (ev.keyCode == 39 || ev.keyCode == 13 || (ev.keyCode == 9 && !ev.shiftKey)) {
+                showNext();
+            }
+            if (ev.keyCode == 37|| (ev.keyCode == 9 && ev.shiftKey)) {
+                showPrev();
+            }
+        }, true);
+        document.addEventListener("click", function (ev) {
+            if (ev.target.classList.contains("nf-start-button")) {
+                showNext();
+            }
+        }, true);
+
+
+        setTimeout(showNext, 10);
+        
+    }
+
+    function move(dir, id,o,n) {
+        var now = dom.gid(id + nowIdx);
+        nowIdx += dir;
+
+        if (now) {
+            now.classList.remove("nf-now");
+            now.classList.add(o);
+        }
+        var next = dom.gid(id + nowIdx);
+
+        next.classList.remove(n);
+        next.classList.add("nf-now");
+    }
+
+
+    function showPrev() {
+        if (nowIdx > 1) {
+            move(-1, "text-", "nf-next", "nf-prev");
+            nowIdx++;
+            move(-1, "input-", "nf-next", "nf-prev");
+        focusNow();
+        }
+    }
+
+    function showNext() {
+        if (nowIdx < questions.length - 1) {
+            move(1, "text-", "nf-prev", "nf-next");
+            nowIdx--;
+            move(1, "input-", "nf-prev", "nf-next");
+        focusNow();
+        }
+    }
+
+    function focusNow() {
+        var now = document.querySelector("#input-" + nowIdx + " input");
+        if (now) {
+            now.focus();
+        }
     }
 
 
     function buildUi() {
-        questions.forEach(function(q, idx) {
+        questions.forEach(function (q, idx) {
+            q.idx = idx;
             var text = Handlebars.templates['tpl-text'](q);
             dom.html(text, document.querySelector('.nf-container .top'));
 
@@ -35,12 +97,12 @@
 
     var questions = [
         {
-            text: "this is the first thing that I want to ask you",
-            type: "string"
+            text: "Before we begin, I need to ask you a few questions",
+            type: "start"
         },
         {
             text: "how many days since the last time you left the state you live in?",
-            type: "number"
+            type: "string"
         },
         {
             text: "What's your favorite TV show?",
@@ -48,7 +110,11 @@
         },
         {
             text: "When were you born?",
-            type: "date"
+            type: "string"
+        },
+        {
+            text: "Thanks! We're all done.",
+            type: "end"
         }
 
     ];
